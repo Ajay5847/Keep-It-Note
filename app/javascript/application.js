129d200fe052b9f0449a6ab4f6c033e085661f22
Ajay Kumar M <ajaybunty91@gmail.com>
2024-08-31 20:07:59 +0530

    WIP: javascript setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,2 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Alpine from "alpinejs";
 import "@hotwired/turbo-rails"
-import "controllers"
-
-window.Alpine = Alpine;
-Alpine.start();
+import "./controllers"
