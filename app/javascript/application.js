48ce5883c28c445d06b7122e89d42bdbe8541d1f
Ajay Kumar M <ajaybunty91@gmail.com>
2024-08-31 12:53:25 +0530

    Integrated alpinejs in app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import Alpine from "alpinejs";
 import "@hotwired/turbo-rails"
 import "controllers"
+
+window.Alpine = Alpine;
+Alpine.start();
